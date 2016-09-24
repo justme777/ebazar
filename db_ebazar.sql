c021281839c31ecc9aaf35c22a62fe49f8ff9b34
justme777 <ereke_enu@mail.ru>
2016-09-25 00:12:48 +0600

    db new version

--- db_ebazar.sql
+++ db_ebazar.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.10
+-- MySQL dump 10.11
 --
 -- Host: localhost    Database: db_ebazar
 -- ------------------------------------------------------
--- Server version	5.0.67-community-nt
+-- Server version	5.5.25
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,69 +20,105 @@
 --
 
 DROP TABLE IF EXISTS `addresses`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `addresses` (
-  `id` int(10) unsigned NOT NULL auto_increment,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `parent_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `addresses`
 --
 
-
-/*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
 LOCK TABLES `addresses` WRITE;
+/*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
 INSERT INTO `addresses` VALUES (2,'test',1),(3,'test',1),(4,'test',1);
-UNLOCK TABLES;
 /*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `market_types`
 --
 
 DROP TABLE IF EXISTS `market_types`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `market_types` (
-  `id` int(10) unsigned NOT NULL auto_increment,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `market_types`
 --
 
-
-/*!40000 ALTER TABLE `market_types` DISABLE KEYS */;
 LOCK TABLES `market_types` WRITE;
-UNLOCK TABLES;
+/*!40000 ALTER TABLE `market_types` DISABLE KEYS */;
 /*!40000 ALTER TABLE `market_types` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `markets`
 --
 
 DROP TABLE IF EXISTS `markets`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `markets` (
-  `id` int(10) unsigned NOT NULL auto_increment,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `address_id` int(10) unsigned NOT NULL,
   `create_date` datetime NOT NULL,
   `user_id` int(10) unsigned NOT NULL,
   `type_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `markets`
 --
 
-
-/*!40000 ALTER TABLE `markets` DISABLE KEYS */;
 LOCK TABLES `markets` WRITE;
-UNLOCK TABLES;
+/*!40000 ALTER TABLE `markets` DISABLE KEYS */;
 /*!40000 ALTER TABLE `markets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `login` varchar(45) DEFAULT NULL,
+  `password_hash` varchar(45) NOT NULL,
+  `create_date` datetime NOT NULL,
+  `adddress_id` int(10) unsigned DEFAULT NULL,
+  `is_blocked` tinyint(1) NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'ereke_enu@mail.ru',NULL,'12345','2016-09-24 22:26:54',NULL,0,0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -93,3 +129,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2016-09-24 18:12:21
