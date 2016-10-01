5787947aa2f5fd6b341a017e4dca6483e120c1e3
justme777 <ereke_enu@mail.ru>
2016-10-01 12:23:05 +0600

    db backup update

--- db_ebazar.sql
+++ db_ebazar.sql
@@ -24,10 +24,10 @@ SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `addresses` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
+  `name` varchar(45) CHARACTER SET latin1 NOT NULL,
   `parent_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -36,8 +36,57 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `addresses` WRITE;
 /*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
-INSERT INTO `addresses` VALUES (2,'test',1),(3,'test',1),(4,'test',1);
+INSERT INTO `addresses` VALUES (2,'Astana',1),(3,'Almaty',1),(4,'Shymkent',1);
 /*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `classifier_values`
+--
+
+DROP TABLE IF EXISTS `classifier_values`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `classifier_values` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `classifier_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `classifier_values`
+--
+
+LOCK TABLES `classifier_values` WRITE;
+/*!40000 ALTER TABLE `classifier_values` DISABLE KEYS */;
+INSERT INTO `classifier_values` VALUES (1,'ТРЦ',1),(2,'Рынок',1),(3,'Магазин',1),(7,'Телефон',2),(8,'Веб-сайт',2),(9,'Электронная почта',2);
+/*!40000 ALTER TABLE `classifier_values` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `classifiers`
+--
+
+DROP TABLE IF EXISTS `classifiers`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `classifiers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `classifiers`
+--
+
+LOCK TABLES `classifiers` WRITE;
+/*!40000 ALTER TABLE `classifiers` DISABLE KEYS */;
+INSERT INTO `classifiers` VALUES (1,'Тип торгового места'),(2,'Контакты Торгого места');
+/*!40000 ALTER TABLE `classifiers` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -49,9 +98,9 @@ SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `market_types` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
+  `name` varchar(45) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -72,13 +121,13 @@ SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `markets` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
+  `name` varchar(45) CHARACTER SET latin1 NOT NULL,
   `address_id` int(10) unsigned NOT NULL,
   `create_date` datetime NOT NULL,
   `user_id` int(10) unsigned NOT NULL,
   `type_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -87,7 +136,55 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `markets` WRITE;
 /*!40000 ALTER TABLE `markets` DISABLE KEYS */;
+INSERT INTO `markets` VALUES (27,'??????',2,'2016-10-01 00:35:45',1,1),(28,'???',2,'2016-10-01 00:36:13',1,1),(29,'?????',2,'2016-10-01 00:40:56',1,1),(30,'???????',2,'2016-10-01 00:45:38',1,1),(31,'?????',2,'2016-10-01 00:48:54',1,1),(32,'?????',2,'2016-10-01 00:52:14',1,1),(33,'???????',2,'2016-10-01 00:52:59',1,1),(34,'??????',2,'2016-10-01 12:05:23',1,1);
 /*!40000 ALTER TABLE `markets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `roles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `user_roles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -129,4 +226,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-24 18:12:21
+-- Dump completed on 2016-10-01  6:22:31
